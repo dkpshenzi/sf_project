4350bd29bcaf08c3e3b966210cfe7400058f12ff
dkp <1017975495@qq.com>
2023-06-18 11:25:36 +0800

    对数据处理方面进行了全面的规整和优化

--- code/main.py
+++ code/main.py
@@ -1,32 +1,15 @@
 import requests
 from uuid import uuid4
-import time
 import os
-import Astar
 import Distribution
 import PathTrack
 import CBS
 import MapCreate
+from PrintControl import PrintControl
 
 SERVER_URL  = os.environ.get("sf-judge-server") or "http://127.0.0.1:5555"
 USERNAME = "算法挑战赛-AI平台"
 SUBMISSION_ID = str(uuid4())
-
-# 事例指令
-'''ACTIONS_SEQ = [
-    [{"type":"PICKUP","dir":"RIGHT"},{"type":"PICKUP","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"STAY"}],
-    [{"type":"MOVE","dir":"UP"},{"type":"STAY"}],
-    [{"type":"STAY"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"STAY"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"DOWN"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"MOVE","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"DELIVERY","dir":"LEFT"}],
-    [{"type":"MOVE","dir":"RIGHT"},{"type":"STAY"}],
-    [{"type":"DELIVERY","dir":"RIGHT"},{"type":"STAY"}]
-]'''
 
 # api设置
 GAMELOAD = 'create_submission'
@@ -34,38 +17,39 @@ MAPSTART = 'start'
 AGVCONTROL = 'step'
 GAMEEND = 'finish_submission'
 
+# 常量的设置
+class PRINT:
+    MAPINFO = 'map_information'     # 信息数据
+    LOG = 'log'                     # 日志
+    ERROR = 'error'                 # 错误
+
 # 类的设置
 class AGV:
-    def __init__(self,agvid,payload,cap,position):
-        self.id = agvid # id
-        self.payload = payload # 负载
-        self.cap = cap    # 容积
-        self.position = position
+    def __init__(self,agvid):
+        self.id = agvid
+        self.payload = None
+        self.position = ()
         self.target = []
         self.path = []
-        self.temp_target = []
-    def update(self,payload,cap,position):
-        self.payload = payload
-        self.cap = cap
-        self.position = position
-
-class CARGO:
-    def __init__(self,position,target):
-        self.position = position
-        self.target = target
-    def update(self,position):
+    def update(self,payload):
         """更新数据
 
         Args:
+            payload (_type_): 负载
             position (_type_): 坐标
         """
-        self.position = position
+        self.payload = payload
+
+class CARGO:
+    def __init__(self,target):
+        self.position = ()
+        self.target = target
         
 class SHELF:
-    def __init__(self,position,index,payload):
-        self.position = position
+    def __init__(self,index):
+        self.position = ()
         self.index = index
-        self.payload = payload
+        self.payload = None
     def update(self,payload):
         """更新数据
 
@@ -73,7 +57,44 @@ class SHELF:
             payload (_type_): 负载值
         """
         self.payload = payload
+        
+class WALL:
+    def __init__(self,position):
+        self.position = position
 
+class MAP:
+    def __init__(self):
+        self.rows = None
+        self.cols = None
+        self.agvs = []
+        self.cargos = []
+        self.shelves = []
+        self.walls = []
+        
+        self.map_grid = None    # 地图创建实例
+        self.dis = None         # 地图创建实例  
+    def set_arr(self,width,height):
+        """设置行数和列数
+
+        Args:
+            rows (_type_): _description_
+            cols (_type_): _description_
+        """
+        self.rows = height  # 行对应高
+        self.cols = width   # 列对应宽
+    def set_instance(self,walls,agvs,cargos,shelves):
+        self.walls = walls
+        self.agvs = agvs
+        self.cargos = cargos
+        self.shelves = shelves
+    def get_map_grid(self):
+        self.map_grid = MapCreate.MapCreate(self.rows,self.cols,self.shelves,self.walls)
+    def get_dis(self):
+        self.dis = Distribution.Distribution(self.cargos,self.shelves)
+    def find_target(self):
+        self.dis.update(self.agvs,self.cargos,self.shelves)
+        self.agvs = self.dis.target_finding()
+    
 def command(api,value={}):
     resp = requests.post(f"{SERVER_URL}/{api}",json=value)
     assert (resp.status_code == 200)
@@ -81,7 +102,7 @@ def command(api,value={}):
     return data
 
 # 处理并提取地图数据，再进行输出
-def sort_map_data(mapdata,is_first):
+def sort_map_data(map_instance,mapdata,is_first):
     """用于整理地图数据，输出到控制台
     arr:
         mapdata: 传入的地图字典数据
@@ -95,113 +116,112 @@ def sort_map_data(mapdata,is_first):
         wall: 墙壁字典列表：x,y
     """
     # 将全部处理过的数据制作成实例的列表
-    
+
     value = mapdata["value"]
-    if is_first:
-        map_attr = value["map_attr"]    # 地图元信息 # 宽，高，最大步数，单次决策最大时间
-        width = map_attr['width']
-        height = map_attr['height']        
-        print(f'地图宽为{width}，高为{height}')
-    
     map_state = value["map_state"]  # 地图状态
+    map_attr = value["map_attr"]    # 地图元信息
     agvs = map_state["agvs"]    # 机器人状态    # id,payload负载,cap
     cargos = map_state["cargos"]    # 货物状态  # id,target目的地货架,weight
     shelves = map_state["shelves"]  # 货架状态  # id,cap,payload
     map_detail = map_state["map"]   # 地图位置信息  # x,y,type,id
-    # type: agvs shelf wall cargo
-    walls = []   # 添加一个障碍物的列表
+    
+    if is_first:
+        # 地图自带的信息
+        width = map_attr['width']
+        height = map_attr['height']        
+        pc.print(f'地图宽为{width}，高为{height}')
+        
+        # 障碍物
+        walls = []
+        for position in map_detail:
+            ty == position['type']
+            if ty == 'wall':
+                x = position['x']
+                y = position['y']
+                wall = WALL((x,y))
+                walls.append(wall)
+                
+        # 机器人实例化
+        agv_li = []
+        for agv in agvs:
+            agv_id = agv['id']
+            new_agv = AGV(agv_id)
+            agv_li.append(new_agv)
+            
+        # 货物实例化
+        cargos_li = []
+        for cargo in cargos:
+            cargo_target = cargo['target']
+            new_cargo = CARGO(cargo_target)
+            cargos_li.append(new_cargo)
+        
+        # 货柜实例化
+        shelves_li = []
+        for shelf in shelves:
+            shelf_id = shelf['id']
+            new_shelf = SHELF(shelf_id)
+            shelves_li.append(new_shelf)
+        
+        map_instance.set_arr(width,height)  # 设置地图宽和高信息
+        map_instance.set_instance(walls,agv_li,cargos_li,shelves_li)       # 设置地图的实例
     
     # 先给所有物品的条目上坐标
     for position in map_detail:
         ty = position['type']
         if ty == 'agv':
-            agvs[position['id']]['x'] = position['x']
-            agvs[position['id']]['y'] = position['y'] 
+            index = position['id']
+            map_instance.agvs[index].position = (position['x'],position['y'] )
         elif ty == 'cargo':
-            cargos[position['id']]['x'] = position['x']
-            cargos[position['id']]['y'] = position['y']
+            index = position['id']
+            map_instance.cargos[index].position = (position['x'],position['y'] )
         elif ty == 'shelf':
-            shelves[position['id']]['x'] = position['x']
-            shelves[position['id']]['y'] = position['y']
-        elif ty == 'wall':
-            wall = {"x":position['x'],'y':position['y']}
-            walls.append(wall)
+            index = position['id']
+            map_instance.shelves[index].position = (position['x'],position['y'] )
     
     # 然后再逐个提取信息
     for agv in agvs:
-        agv_id = agv['id']
+        index = agv['id']
         agv_payload = agv['payload']
-        agv_cap = agv['cap']
-        agv_position = (agv['x'],agv['y'])
-        if is_first:
-            new_agv = AGV(agv_id,agv_payload,agv_cap,agv_position)
-            AGV_li.append(new_agv)
-        print(f'机器人编号：{agv_id}，负载：{agv_payload}，cap：{agv_cap}，位置：{agv_position}')
+        map_instance.agvs[index].update(agv_payload)
     
-    new_cargos = []
-    for cargo in cargos:
-        # 先判断是否存在坐标，若无坐标则去除
-        new_cargos.append(cargo)
-        cargo_id = cargo['id']
-        cargo_target = cargo['target']
-        cargo_weight = cargo['weight']
-        if 'x' not in cargo.keys():
-            cargo['x'] = None    
-            cargo['y'] = None
-        cargo_position = (cargo['x'],cargo['y'])
-        print(f"货物编号：{cargo_id}，目标：{cargo_target}，重量：{cargo_weight}，位置：{cargo_position}")
-    print()
-    
-    if is_first:
-        return map_attr,agvs,new_cargos,shelves,walls
-    else:
-        # print(cargos)
-        return agvs,new_cargos,shelves
+    for shelf in shelves:
+        index = shelf['id']
+        shelf_payload = shelf['payload']
+        map_instance.shelves[index].update(shelf_payload)
+        
+    return map_instance
         
 if __name__ == "__main__":
-    # 通过对网站发送post命令来获取信息
-    # 初始化接受地图数据
     # 获得地图id列表,maps:[]
     data = command(GAMELOAD)
+    
+    # 实例化打印控制器
+    pc = PrintControl()
 
     # 每个地图展开
     for map_id in data['value']['maps']:
-        print(f'当前地图为：{map_id}')
+        pc.print(f'当前地图为：{map_id}',PRINT.MAPINFO)
         
-        # 每张地图开始初始化机器人列表
-        AGV_li = []
+        # 实例化当前地图
+        MA = MAP()
         
         # 指定一个地图然后开始比赛
-        # 获得地图数据
-        # 这个是刚刚开始的数据
         mapdata = command(MAPSTART,{"map_id":map_id})
-        # 不仅仅得到地图的数据，第一次读取地图数据还会得到机器人的类列表
-        map_attr,agvs,cargos,shelves,walls = sort_map_data(mapdata,True)    # 这里是第一次读取地图的数据
         
-        # 得到长和宽
-        rows = map_attr['height']
-        cols = map_attr['width']
+        # 第一次读取地图数据
+        MA = sort_map_data(MA,mapdata,True)    
         
         # 初始化地图
-        map_grid = MapCreate.MapCreate(rows,cols,shelves,walls)
+        MA.get_map_grid()
         
-        # 处理地图数据
-        # 包括规划路线
-        # agvs: id,payload,cap,(x,y),(target1,target2)
-        # 这里是传入AGV_li 列表，然后改变AGV_li列表中的AGV目标
-        dis = Distribution.Distribution(cargos,shelves)
-        # 更新数据，并且进行目标的寻找
-        dis.update(AGV_li,cargos,shelves)
-        AGV_li,is_change_path = dis.target_finding()
-        
-        # print(f'是否更新了目标：{is_change_path}')
-        
-        # 这里是通过AGV列表中的AGV类的目标进行路径规划，总共规划两条路线
-        # 得到每个机器人的一个路线
-        # 首先循环每个机器人的目标
-        # 这里只能够先完成第一阶段的一个寻路
-        cbs = CBS.CBS(AGV_li,shelves,cargos,map_grid)
-        AGV_li = cbs.solve()
+        # 规划目标
+        MA.get_dis()
+        # 分配目标
+        MA.find_target()
+
+        # 找路径
+        cbs = CBS.CBS(MA.agvs,MA.map_grid)
+        MA.agvs = cbs.solve()
             
         # 初始化步数
         step = 0
@@ -211,10 +231,7 @@ if __name__ == "__main__":
             command_li = []
             
             # 一张地图内，指令的输出
-            # 首先输出指令，然后获取是否抵达目的地
-            for agv in AGV_li:
-                # print(f'当前目标为：{agv.target}')
-                
+            for agv in MA.agvs:
                 pathTrack = PathTrack.PathTrack(agv)
                 c = pathTrack.path_tracking()
                 if c['type'] == 'DELIVERY':
@@ -222,46 +239,26 @@ if __name__ == "__main__":
                     agv.path = []
                 command_li.append(c)
             
-            # print(command_li)
-            
             # 开始输出指令
             data = command(AGVCONTROL,{"map_id": map_id,"actions": command_li})
-            
-            # sort_map_data(data,False)
             
             # 判断是否已经完成地图
             try:
                 if data["value"]["done"] or step > 30:
                     break
             except:
-                print(f'数据为：{data}')
+                pc.print(f'数据为：{data}',PRINT.ERROR)
             
             # 首先进行数据的更新
-            agvs,cargos,shelves = sort_map_data(data,False)
-            for index,agv in enumerate(agvs):
-                a = AGV_li[index]
-                a.update(agv['payload'],agv['cap'],(agv['x'],agv['y']))
+            MA = sort_map_data(MA,mapdata,False) 
             
             # 完成后进行目标的重新选择
-            dis.update(AGV_li,cargos,shelves)
-            # print(f'cargos:{cargos}')
-            # print(f'shelves:{shelves}')
-            AGV_li,is_change_path = dis.target_finding()
+            MA.find_target()
             
-            for agv in AGV_li:
-                print(f'当前目标为：{agv.target}')
-                print(f'当前路径为：',end='')
-                for node in agv.path:
-                    print(f'{node.position}',end='')
-                print()
-            
-            # 如果目标发生了改变，则需要重新规划
-            # 这个时候再进行一次路径规划，只进行货物到货柜的路径规划
-            cbs = CBS.CBS(AGV_li,shelves,cargos,map_grid)
-            AGV_li = cbs.solve()
-            
-            # print(f'完成了一次规划')
-            
+            # 需要重新规划
+            cbs = CBS.CBS(MA.agvs,MA.map_grid)
+            MA.agvs = cbs.solve()
+
             step += 1
     
     # 输入结束指令
